98ea2d04311cb8bee36f1b4c682847ca49a5c354
igorstar <contactigorstar@gmail.com>
2015-06-24 19:45:12 -0700

    Initial Commit

---
+++ Pitch Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Project
+//
+//  Created by IgorStar on 6/24/15.
+//  Copyright (c) 2015 IgorStar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
